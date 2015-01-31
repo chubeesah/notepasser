cfa87e2c91ee682865ec1f062b3c66dbc8719034
Chubes <Chubes@andrew-mercers-macbook.local>
2015-01-30 19:29:23 -0500

    first draft messenger

--- lib/notepasser.rb
+++ lib/notepasser.rb
@@ -3,13 +3,69 @@ require "notepasser/init_db"
 require "camping"
 require "pry"
 
+#IP: 10.0.0.84
+
 Camping.goes :Notepasser
 
 module Notepasser
 end
 
 module Notepasser::Models
+  
+  class Users < ActiveRecord::Base
+  end
+
+  class Messages < ActiveRecord::Base
+  end
 end
 
 module Notepasser::Controllers
+  class UserId < R '/users'
+    def get(id)
+      user = Notepasser::Models::Users.find(id)
+      user.to_json
+    end
+
+    def put
+      new_user = Notepasser::Models::Users.create(id)
+      new_user.to_json
+    end
+
+    def delete
+      delete_user = Notepasser::Models::Users.find(id)
+      delete_user.destroy
+    end
+  end  
+
+  class MessageId < R '/messages/(\d+)'
+    def get(id)
+      message = Notepasser::Models::Messages.find(id)
+      message.to_json 
+    end
+
+    def put
+      new_message = Notepasser::Models::Messages.create(@input)
+      new_message.to_json
+    end
+
+    def delete(id)
+      delete_message = Notepasser::Models::Messages.find(id)
+      delete_message.destroy
+    end
+  end  
+  class MessageController < R '/messages'
+    def get
+    end
+
+    def put
+    end
+
+    def delete
+    end
+  end
 end
+
+
+
+
+
